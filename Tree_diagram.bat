1750d5c92894fda243df250b4f0919ce266106ee
Ryan Dreifuerst <dreifuerstrm@msoe.edu>
2019-01-22 20:58:37 -0600

    RandomForest Images

---
+++ Tree_diagram.bat
@@ -0,0 +1,6 @@
+cd C:\Users\dreifuerstrm\SeniorDesign\Code\CPU
+dot -Tpng Wrist_Rescue_0.dot -o Wrist_rescue_0.png
+dot -Tpng Wrist_Rescue_1.dot -o Wrist_rescue_1.png
+dot -Tpng Wrist_Rescue_2.dot -o Wrist_rescue_2.png
+dot -Tpng Wrist_Rescue_3.dot -o Wrist_rescue_3.png
+dot -Tpng Wrist_Rescue_4.dot -o Wrist_rescue_4.png
